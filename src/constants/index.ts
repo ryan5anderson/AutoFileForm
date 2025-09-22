b50a7d1b0b2600dd90f5a2d2d1ffdd7e09075aa9
Ryan Anderson <anderson.t.ryan@gmail.com>
2025-09-22 16:05:17 -0400

    Updates

--- src/constants/index.ts
+++
@@ -1,5 +0,0 @@
-export const EMAILJS_SERVICE_ID = 'service_ehf05es';
-export const EMAILJS_TEMPLATE_ID = 'template_t1138kn';
-export const EMAILJS_USER_ID = 'f7BZO-3hAYB-QusoW';
-export const PROVIDER_EMAIL = 'anderson.t.ryan@gmail.com';
-// categories are now in colleges.ts 
